50c93df33f5ee0db23152332409f5a2729804532
Atikmamu <robinatik50@gmail.com>
2024-09-23 19:28:31 +0600

    added utilities.js file and features.js file done

--- js/utilities2.js
+++ js/utilities2.js
@@ -10,3 +10,16 @@ function getTextFieldValueById(id){
     const TextNumber = parseFloat(TextValue);
     return TextNumber; 
 }
+
+function showSectionById(id){
+
+    // hide all the sections
+
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    // show the section with the provide id as parameter 
+
+    document.getElementById(id).classList.remove('hidden');
+}
