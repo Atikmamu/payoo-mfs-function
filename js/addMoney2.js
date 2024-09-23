ae6adc5c6c1152d7f623294b18f0667ae1ae9bf9
Atikmamu <robinatik50@gmail.com>
2024-09-23 21:25:34 +0600

    validation done

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -1,17 +1,38 @@
 document.getElementById("btn-add-money")
     .addEventListener("click", function (event) {
-        event.preventDefault();
+        event.preventDefault();   //prevent hosse 
 
         const addMoney = getInputFieldValueById('input-add-money');
         const pinNumber = getInputFieldValueById('input-pin-number');
+
+
+         // validation cheak NaN ...> Transactions 
+        if(isNaN(addMoney)){
+            alert('Failed to add money');
+            return;
+        }
         
 
-        // wrong way to verify. do not try it at your serious website 
+        // wrong way to verify. do not try it at your serious website...Validate . 
         if(pinNumber === 1234){
             const balance = getTextFieldValueById('account-balance');
-            const newBalance = balance + addMoney;
+            const newBalance = balance + addMoney;   // balance a add kora falchi 
+
+            // uI te dekhai falsi ..>
 
             document.getElementById('account-balance').innerText = newBalance;
+
+
+            // add to transaction history ...> p tag diye.> tamplate sting `${}` diye korbo 
+             // `Added: ${ atar moddhe ja kono variable ar man rhakte parbo }`
+
+            const p = document.createElement('p');
+            p.innerText = `Added: ${addMoney} TK. New Balance: ${newBalance}`;  // addMoney ka dynamic vabe likhar jonno 
+            console.log(p);
+
+            //  should be a common function
+            document.getElementById('transaction-container').appendChild(p);
+
         }
         else{
             alert('Failed to add the money.');
--- js/cashOut.js
+++ js/cashOut.js
@@ -7,13 +7,48 @@ document.getElementById('btn-cash-out')
 
     // console.log('inside the click handler', cashOut, pinNumber);
 
+    // validation cash out Nan 
+    
+    if(isNaN(cashOut)){
+        alert('Failed to cash out.');
+        return;
+    }
+
+
+
     if(pinNumber === 1234){
          const balance = getTextFieldValueById('account-balance');
+
+         // validation check taka bashi cashout korla -2444 na dekhai 
+
+         if(cashOut > balance){
+            alert('you do not have enough money to cash out');
+            return;
+         }
+
+
+
          const newBalance = balance - cashOut;
          document.getElementById('account-balance').innerText = newBalance;
+
+         // add to transaction history 
+         const div = document.createElement('div');
+         div.classList.add('bg-yellow-300');
+         div.innerHTML = `
+              <h4 class="text-2xl font-bold">Cash Out</h4>
+              <p>${cashOut} withdraw. New Balance ${newBalance}</p>
+         
+         `
+
+         document.getElementById('transaction-container').appendChild(div);
+
+
     }
     else{
         alert('No money for you.... DGM.');
     }
 
-})
+});
+
+
+
